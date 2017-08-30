02ef968675cdac520d0483943c151df2a7018a49
Roger Jaffe <rogerjaffe@gmail.com>
2017-08-30 06:40:23 -0700

    Initial commit

---
+++ Die6.java
@@ -0,0 +1,32 @@
+
+/**
+ * Abstracts one die (plural is dice)
+ * 
+ * @Mr. Jaffe
+ * @1.0 2017-07-13
+ */
+public class Die6
+{
+  private int value;
+
+/**
+ * Constructor to do an initial roll to set the first value
+ */
+  public Die6() {
+    this.roll();
+  }
+  
+/**
+ * Getter for value
+ */
+  public int getValue() {
+    return value;
+  }
+
+/**
+ * Roll the die! Generate random number 1 <= x <= 6 and assign to value
+ */
+  public void roll() {
+    this.value = (int)(Math.random() * 6) + 1;
+  }
+}
