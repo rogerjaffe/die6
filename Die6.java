e8b67bead0b79ac01529a485fbf6f8ddae422da0
Roger Jaffe <rogerjaffe@gmail.com>
2019-03-27 15:47:04 -0700

    Added rollAndGetValue; Added documentation

--- Die6.java
+++ Die6.java
@@ -12,24 +12,37 @@ public class Die6
    */
   private int value;
 
-/**
- * Constructor to do an initial roll to set the first value
- */
+  /**
+   * Constructor to do an initial roll to set the first value
+   */
   public Die6() {
     this.roll();
   }
-  
-/**
- * Getter for value
- */
+
+  /**
+   * Getter for value
+   * @return Die value
+   */
   public int getValue() {
     return value;
   }
 
-/**
- * Roll the die! Generate random number 1 <= x <= 6 and assign to value
- */
+  /**
+   * Roll the die! Generate random number 1 <= x <= 6 and assign to value
+   * Note that roll does NOT return the new value
+   */
   public void roll() {
     this.value = (int)(Math.random() * 6) + 1;
   }
+
+  /**
+   * Roll the die and return the new value
+   * @return Die value
+   */
+  public int rollAndGetValue() {
+    roll();
+    return getValue();
+  }
+
 }
+
